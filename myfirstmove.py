2a8f78620aa91530b86cb9cfa887c339a843b92e
Luca Ferroni <luca@befair.it>
2017-05-17 17:30:13 +0200

    [MOVE] il mio primo movimento
    Principalmente copiato da:
    http://doc.aldebaran.com/2-1/naoqi/motion/control-walk-api.html#ALMotionProxy::setMoveArmsEnabled__bCR.bCR

---
+++ myfirstmove.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+
+from naoqi import ALProxy
+import time
+
+NAO_IP = '192.168.10.10'
+NAO_PORT = 9559
+
+motionProxy = ALProxy("ALMotion", NAO_IP, NAO_PORT)
+postureProxy = ALProxy("ALRobotPosture", NAO_IP, NAO_PORT)
+postureProxy.goToPosture("StandInit", 0.5)
+
+x          = 0.6
+y          = 0.0
+theta      = 0.0
+frequency  = 1.0
+motionProxy.setWalkTargetVelocity(x, y, theta, frequency)
+
+time.sleep(4.0)
+
+motionProxy.stopMove()
+
