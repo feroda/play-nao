178d8a176a7b4b2689b8331f25cfa6306a199d6b
Luca Ferroni <luca@befair.it>
2017-05-17 10:52:05 +0200

    [HELLO] modificato il saluto iniziale per renderlo impersonato dal robot

--- myfirstwords.py
+++ myfirstwords.py
@@ -1,4 +1,4 @@
 from naoqi import ALProxy
 tts = ALProxy("ALTextToSpeech", '192.168.1.12', 9559)
 tts.say("Hello, world!")
-tts.say("Ciao a tutti dal prof Ferroni, e grazie a Davide e Lorenzo per avermi presentato!")
+tts.say("Grazie Davide e Lorenzo per avermi presentato, un saluto dal prof Ferroni!")
