dbf5d8edc7159891824403b757e550b34afd4d00
Luca Ferroni <luca@befair.it>
2017-05-17 10:51:20 +0200

    [SETVOLUME] resi parametrici IP e livello di volume

--- setvolume.py
+++ setvolume.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
+
+import sys
 from naoqi import ALProxy
 
-IP_NAO = '192.168.10.10'
-tts = ALProxy("ALTextToSpeech", IP_NAO, 9559)
-tts.say("Ciao quinto B, mi abbasso il volume")
-tts.setVolume(0.1)
-tts.say("Ora il volume è al 10% scusate il disturbo per prima!")
+def main(robotIp, volume):
+
+    tts = ALProxy("ALTextToSpeech", robotIp, 9559)
+    tts.say("Ciao quinto B, mi abbasso il volume")
+    tts.setVolume(volume)
+    tts.say("Ora il volume è al {}%% scusate il disturbo per prima!".format(volume*100))
+
+
+if __name__ == "__main__":
+    robotIp = "127.0.0.1"
+    volume = 0.2
+
+    if len(sys.argv) > 1:
+        robotIp = sys.argv[1]
+        if len(sys.argv) == 3:
+            try:
+                volume = float(sys.argv[2])
+            except TypeError:
+                print('Volume deve essere espresso in forma decimale tra 0 e 1 con separatore "."')
+    else:
+        print "Usage python almotion_rest.py <robotIP> <volume percent float 0-1> (optional default: 127.0.0.1 0.2)"
+
+    main(robotIp, volume)
