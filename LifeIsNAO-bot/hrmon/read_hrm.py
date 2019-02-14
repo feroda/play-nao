82acbc8e9804dca9d156212b106ccf5a81b5cef3
Luca Ferroni <luca@befair.it>
2019-02-14 22:54:27 +0100

    Do not crash if device disconnects!

--- LifeIsNAO-bot/hrmon/read_hrm.py
+++ LifeIsNAO-bot/hrmon/read_hrm.py
@@ -10,15 +10,19 @@ if not os.path.exists("/var/lib/nao-debian/"):
     os.mkdir("/var/lib/nao-debian")
 
 while True:
-    mx30.read_sensor()
-    print("ir=%s red=%s" % (mx30.ir, mx30.red))
-    if mx30.ir >= 1000:
-        with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
-            f.write(str(mx30.ir))
-        with open("/var/lib/nao-debian/dev/shm/hrm_buffer.txt", "w") as f:
-            f.write("\n".join(map(str, mx30.buffer_ir[:-20])))
-    else:
-        with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
-            f.write("0")
+    try:
+        mx30.read_sensor()
+        print("ir=%s red=%s" % (mx30.ir, mx30.red))
+        if mx30.ir >= 1000:
+            with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
+                f.write(str(mx30.ir))
+            with open("/var/lib/nao-debian/dev/shm/hrm_buffer.txt", "w") as f:
+                f.write("\n".join(map(str, mx30.buffer_ir[:-20])))
+        else:
+            with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
+                f.write("NODATA")
 
-    time.sleep(0.5)
+        time.sleep(0.5)
+    except IOError as e:
+        with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
+            f.write("ERROR")
