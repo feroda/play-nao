bd128f4ac32935fdb102b45bb756dd582d7b6671
Luca Ferroni <luca@befair.it>
2019-02-18 15:10:34 +0100

    FIXED: sistemato il merge fatto velocemente
    e che presentava classi e funzioni ripetute (!)

--- LifeIsNAO-bot/hellonao.py
+++ LifeIsNAO-bot/hellonao.py
@@ -109,11 +109,11 @@ def fileread(bot, update):
       line = F.readline()
       if line != 'NODATA' and line != 'ERROR':
         value = float(line)/160
-        
+
         update.message.reply_text(value)
 
         msg = 'Il valore è {}'.format(
-        value  
+        value
         ).replace('.', ',')
 
         tts = ALProxy("ALTextToSpeech", NAO_IP, NAO_PORT)
@@ -127,11 +127,11 @@ def fileread_media(bot, update):
         if line != 'NODATA' and line != 'ERROR':
           value = float(line)/160
           i = i+1
-        #inserrire eventuale delay
+        #inserire eventuale delay
         update.message.reply_text(value)
 
         msg = 'Il valore è {}'.format(
-        value  
+        value
         ).replace('.', ',')
 
 class HumanAnsweredQuestionModule(ALModule):
@@ -192,7 +192,7 @@ class HumanAnsweredQuestionModule(ALModule):
         #i = 0
         last_frequencies = "" # TODO scrivere funzione fileread_sensor_last_values(n=10)
         msg += " * ULTIME: %s\n" % last_frequencies
-       
+
 
         # Inviare il messaggio al BOT
         # TODO bot.send_message(chat_id, msg)
@@ -204,7 +204,7 @@ class HumanAnsweredQuestionModule(ALModule):
         # OLD     "onFaceDetected")
 
 def main():
-    
+
     # Parse Args
     parser = argparse.ArgumentParser()
     parser.add_argument('--ip', default=NAO_IP)
@@ -243,97 +243,6 @@ def main():
 # nella funzione o nel modulo chiamato come callback si fa
 #bot.send_message(chat_id, msg)
 # dove msg è il testo recuperato dalla memoria di NAO
-
-
-class HumanAnsweredQuestionModule(ALModule):
-    """ A simple module able to react
-    to facedetection events
-
-    """
-    def __init__(self, name):
-        ALModule.__init__(self, name)
-        # No need for IP and port here because
-        # we have our Python broker connected to NAOqi broker
-
-        # Create a proxy to ALTextToSpeech for later use
-        self.tts = ALProxy("ALTextToSpeech")
-
-        # Subscribe to the FaceDetected event:
-        global memory
-        memory = ALProxy("ALMemory")
-        memory.subscribeToEvent("AnswerGiven",
-            "HumanAnsweredQuestion",
-            "onAnswerGiven")
-
-        self.answers = []
-        # Inserire qui le domande vere
-        self.questions = [
-            "Quanti hanni hai?",
-            "Hai mangiato oggi?",
-        ]
-
-    def onAnswerGiven(self, *args):
-        """ This will be called each time an answer is given
-        """
-        # OLD Unsubscribe to the event when talking,
-        # OLD to avoid repetitions
-        # OLD memory.unsubscribeToEvent("FaceDetected",
-        # OLD     "HumanAnsweredQuestion")
-
-
-<<<<<<< HEAD
-        print("Rilevata risposta")
-        msg = ""
-        for i in range(len(self.questions)):
-            try:
-                question = self.questions[i-1]
-                answer = memory.GetData("Domanda/%s" % i)
-                msg += u"* Q:%s A:%s\n" % (question, answer)
-            except Exception as e:
-                # Qui ci va alla prima chiave che non esiste
-                # Esce dal ciclo
-                break
-
-        # Leggere il valore istantaneo dal sensore
-        freq_value = ""  # TODO  scrivere funzione fileread_sensor()
-        msg += " * FREQUENZA: %s\n" % freq_value
-
-        # Leggere gli ultimi 10 valori dal sensore
-        last_frequencies = ""  # TODO scrivere funzione fileread_sensor_last_values(n=10)
-        msg += " * ULTIME: %s\n" % last_frequencies
-
-        # Inviare il messaggio al BOT
-        # TODO bot.send_message(chat_id, msg)
-
-        # OLD Subscribe again to the event
-        # OLD memory.subscribeToEvent("FaceDetected",
-        # OLD     "HumanAnsweredQuestion",
-        # OLD     "onFaceDetected")
-
-def main():
-
-    # Parse Args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--ip', default=NAO_IP)
-    parser.add_argument('--port', default=NAO_PORT, type=int)
-    # Enable or disable naoqi
-    # parser.add_argument('--naoqi', action="store_true", default=False)
-    global sockinfo
-    sockinfo = parser.parse_args()
-
-    # Start NAO Broker
-    myBroker = ALBroker("myBroker", "0.0.0.0", 0, sockinfo.ip, sockinfo.port)
-    # Warning: HumanAnsweredQuestion must be a global variable
-    # The name given to the constructor must be the name of the
-    # variable
-    global HumanAnsweredQuestion
-    HumanAnsweredQuestion = HumanAnsweredQuestionModule("HumanAnsweredQuestion")
-
-    # Start BOT
-    def terminate(self, signal_name):
-        print("Interrupted bot and NAO module by user, shutdown")
-        myBroker.shutdown()
-        sys.exit(0)
 
     updater = Updater(TOKEN, user_sig_handler=terminate)
     updater.dispatcher.add_handler(CommandHandler('naohello', hello))
