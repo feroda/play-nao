115c2194883490760f725b195490220ed165e474
Luca Ferroni <luca@befair.it>
2019-04-05 16:26:31 +0200

    Modifiche alla lettura dati dal sensore per creare grafico

--- LifeIsNAO-bot/hrmon/read_hrm.py
+++ LifeIsNAO-bot/hrmon/read_hrm.py
@@ -3,11 +3,32 @@
 import max30100
 import time
 import os
+import matplotlib.pyplot as plt
 
+BUFFER_FILEPATH = '/var/lib/nao-debian/dev/shm/hrm_buffer.txt'
+GRAFICO_FILEPATH = '/var/lib/nao-debian/dev/shm/grafico.png'
 mx30 = max30100.MAX30100()
 
 if not os.path.exists("/var/lib/nao-debian/"):
     os.mkdir("/var/lib/nao-debian")
+
+
+def scrivi_grafico():
+    
+    # x = [1,2,3,4,5,6,7,8,9,10]
+    # y = [1,2,3,4,5,6,7,8,9,10]
+
+    y = mx30.buffer_ir[-10:]
+    x = range(1, len(y)+1)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    ax.set(xlabel='time (s)', ylabel='battiti (bpm)', title='Monitor cardiaco')
+    ax.grid()
+
+    fig.savefig(GRAFICO_FILEPATH)
+
 
 while True:
     try:
@@ -16,8 +37,10 @@ while True:
         if mx30.ir >= 1000:
             with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
                 f.write(str(mx30.ir))
-            with open("/var/lib/nao-debian/dev/shm/hrm_buffer.txt", "w") as f:
+            with open(BUFFER_FILEPATH, "w") as f:
+                print(mx30.buffer_ir[-20:])
                 f.write("\n".join(map(str, mx30.buffer_ir[-20:])))
+            scrivi_grafico()
         else:
             with open("/var/lib/nao-debian/dev/shm/hrm_instant.txt", "w") as f:
                 f.write("NODATA")
