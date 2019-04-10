8b28bc31b684886d548821c2d3ea81e915d40cfc
Guramrit Singh <guramrit.singh@iismerlonimiliani.it>
2019-04-10 10:21:20 +0200

    steps-hrmon.sh: istruzioni di esecuzione e sleep 3

--- LifeIsNAO-bot/steps-hrmon.sh
+++ LifeIsNAO-bot/steps-hrmon.sh
@@ -3,6 +3,12 @@
 # Il processo hrmon ha bisogno delle librerie grafiche.
 # Perciò è necessario eseguire read_hrm.py da utente "pi"
 # dopo essere entrati nel raspberry con ssh -X pi@ip-del-raspberry
+# ed aver lanciato tmux
+# I comandi da eseguire sono dunque:
+#
+# ssh -X pi@ip-del-raspberry
+# tmux
+# steps-hrmon.sh
 
 if [ "$DISPLAY" = "" ]; then
     echo "Per eseguire questo comando hai bisgno della variabile DISPLAY";
@@ -13,7 +19,7 @@ fi
 sudo chmod -R 777 /dev/shm/
 cd /usr/local/play-nao/LifeIsNAO-bot/hrmon/
 python read_hrm.py &
-sleep 1
+sleep 3
 
 cat /dev/shm/hrm_instant.txt
 if [ $? -eq 0 ]; then
