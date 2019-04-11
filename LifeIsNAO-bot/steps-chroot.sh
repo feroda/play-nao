fe9a1677a4a44bb5a9165e8449e2cfb1e51562fe
Guramrit Singh <guramrit.singh@iismerlonimiliani.it>
2019-04-11 19:33:50 +0200

    tolto devpts nel chroot

--- LifeIsNAO-bot/steps-chroot.sh
+++ LifeIsNAO-bot/steps-chroot.sh
@@ -15,14 +15,11 @@ for d in sys dev proc run; do
 	mount -o bind /$d /var/lib/nao-debian/$d
 done
 mount -o bind /dev/shm /var/lib/nao-debian/dev/shm
-mount -o bind /dev/pts /var/lib/nao-debian/dev/pts
 
 chroot /var/lib/nao-debian/
 
 sleep 1
 umount /var/lib/nao-debian/dev/shm
-sleep 1
-umount /var/lib/nao-debian/dev/pts
 sleep 1
 for d in sys dev proc run; do
 	umount /var/lib/nao-debian/$d
